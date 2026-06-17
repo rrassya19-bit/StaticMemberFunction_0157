72e9e84219d1ee46524b60eab583ab4ed3ed83ee
rrassya19-bit <rrassys19@gmail.com>
2026-06-17 22:46:32 +0700

    Inisialisasi class Kendaraan dengan deklarasi constructor overloading

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Kendaraan {
+private:
+    int idKendaraan;
+    string namaKendaraan;
+
+public:
+    Kendaraan();                                   // Constructor tanpa parameter (default)
+    Kendaraan(int iId);                             // Constructor hanya dengan id
+    Kendaraan(string iNama);                        // Constructor hanya dengan nama
+    Kendaraan(int iId, string iNama);                // Constructor dengan parameter lengkap
+    void tampilkanInfo();
+};
+
+int main() {
+    return 0;
+}
