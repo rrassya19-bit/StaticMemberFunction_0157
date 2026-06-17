598bf8c731e8a590e5f2ef1735901ea9e9af3e87
rrassya19-bit <rrassys19@gmail.com>
2026-06-17 22:40:04 +0700

    Inisialisasi class mahasiswa dengan deklarasi constructor overloading

--- ConstructorOverLoading.cpp
+++ ConstructorOverLoading.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+int main() {
+    return 0;
+}
