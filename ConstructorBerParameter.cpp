debaf82ac00e8b1e2e141a34a7117112989191f2
rrassya19-bit <rrassys19@gmail.com>
2026-06-17 22:38:10 +0700

    Inisialisasi class mahasiswa dengan deklarasi constructor berparameter

--- ConstructorBerParameter.cpp
+++ ConstructorBerParameter.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(int nim, string nama);  // constructor dengan parameter
+};
+
+int main(){
+    return 0;
+}
